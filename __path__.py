4bab0bfed18b4dc5ea800dbda3a58df68e647b09
agrorony <rony.schwartz@mail.huji.ac.il>
2026-04-21 15:57:54 +0200

    Update colab_nnUNet_pipeline.ipynb

--- __path__.py
+++ __path__.py
@@ -4,6 +4,6 @@ PATH_nnUNet_raw = r"C:\nnUNet_workspace\nnUNet_raw"
 input_dir_images = r"C:\nnUNet_workspace\temp_nlm_images"
 input_dir_masks = r"C:\nnUNet_workspace\temp_nlm_annotations"
 PATH_ImageJ = ""  # Fiji not required on this machine
-PATH_nnUNet_raw= r"D:\nnUNet_workspace\nnUNet_raw"
+PATH_nnUNet_raw= r"C:\Users\rony.schwartz\Documents\nnUNet_resources\bnei_reem\nnUNet_raw"
 input_dir_images = r"C:\Users\rony.schwartz\Documents\nnUNet_resources\bnei_reem\images"
 input_dir_masks = r"C:\Users\rony.schwartz\Documents\nnUNet_resources\bnei_reem\annotations"
